a3dc79d51aec58e9c441d8194139dce8efec060a
Sebastian Schmidt <s.schmidt@uni-leipzig.de>
2023-06-07 10:11:23 +0200

    Implemented affinity propagation

--- Group4/clustering/affinity_propagation.py
+++ Group4/clustering/affinity_propagation.py
@@ -1,27 +1,100 @@
 import numpy as np
 from numpy.typing import NDArray
-from typing import Optional
+from typing import Optional, Tuple
 
 from utils import squared_euclidean_distance
 
+def affinity_propagation(X: NDArray, damping: float = 0.5, max_iter: int = 200, convergence_iter: int = 15,
+                         precomputed: Optional[NDArray] = None, preferences: Optional[NDArray] = None,
+                         verbose: bool = False) -> Tuple[NDArray, NDArray, NDArray]:
+    """Perform clustering on the point array `X` using the Affinity Propagation [1] algorithm.
+    Parameters:
+        X:                      Two-dimensional array of shape `(n_samples, n_features)`
+        damping:                Damping factor to avoid numerical oscillations between iterations.
+                                0: Only new responsibility values will be used
+                                1: Only old responsibility values will be used (no update)
+                                Recommended interval is [0.5, 1.0)
+        max_iter:               Upper limit for number iterations for message passing
+        convergence_iter:       Upper limit for number of iterations without change in labels
+        precomputed:            Optional array of shape (n, n) to provide precomputed similarities
+        preferences:            Optional array of shape (n, ); If None, the median of all similarities is used
+        verbose:                True: Print number of iterations
 
-def compute_responsibility(similarities_mat: NDArray,
-                           responsibilities_mat: NDArray,
-                           availabilities_mat: NDArray,
-                           damping: float = 0.5) -> NDArray:
-    """ Computes values for the responsibility messages for the next iteration.
-        Parameters:
-            similarities_mat:       Array of similarity values; Shape `(n, n)`
-            responsibilities_mat:   Array of responsibilities from previous iteration; Shape `(n, n)`
-            availabilities_mat:     Array of availabilities from previous iteration; Shape `(n, n)`
-            damping:                Damping factor to avoid numerical oscillations between iterations.
-                                    0: Only new responsibility values will be used
-                                    1: Only old responsibility values will be used (no update)
-                                    Recommended range [0.5, 1.0)
-        Returns:
-            Array of shape `(n, n)` containing the new responsibility values.
+    Returns:
+        cluster_centers_indices:    Array of indices of the points used as cluster centers (exemplars)
+        cluster_centers             Array of coordinates of the points used as cluster centers (exemplars)
+        final_labels                Array of shape `(n, )` with each point being assigned a cluster center (exemplar)
+    References:
+        [1] B. J. Frey and D. Dueck. Clustering by Passing Messages Between Data Points.
+            In: Science 315, pp. 972-976 (2007).DOI:10.1126/science.1136800
     """
+    assert 0 <= damping <= 1, print('The damping factor must be in [0, 1]; The recommended interval is [0.5, 1.0)')
 
+    # 1. Preparation: Calculate similarities, set optional preferences, and initialize message values
+    similarities_mat, responsibilities_mat, availabilities_mat = prepare_matrices(X=X,
+                                                                                  precomputed=precomputed,
+                                                                                  preferences=preferences)
+
+    # 2. Loop for max iterations or until no change was recorded for convergence_iter
+    i, iter_no_change = 0, 0
+    for i in range(max_iter):
+        # Calculate the scores to find current labels
+        score = responsibilities_mat + availabilities_mat
+        labels = np.argmax(score, axis=1)
+
+        # Update message values
+        responsibilities_mat = update_responsibility(similarities_mat, responsibilities_mat, availabilities_mat,
+                                                     damping=damping)
+        availabilities_mat = update_availability(responsibilities_mat, availabilities_mat, damping=damping)
+
+        # Update labels and iterations without a change
+        new_score = responsibilities_mat + availabilities_mat
+        new_labels = np.argmax(new_score, axis=1)
+
+        if np.all(new_labels == labels):
+            iter_no_change += 1
+        else:
+            iter_no_change = 0
+
+        # If the convergence iterations are reached, break the loop
+        if iter_no_change >= convergence_iter:
+            break
+
+    if verbose:
+        msg = f"Converged after {i} iterations." if i < (max_iter - 1) else \
+            f"Reached {max_iter} iterations without convergence."
+        print(msg)
+
+    # 3. Finalize
+    # 3a. Assign points to the final cluster_centers (exemplars)
+    final_scores = responsibilities_mat + availabilities_mat
+    final_labels = np.argmax(final_scores, axis=1)
+    cluster_centers_indices = np.unique(final_labels)
+    cluster_centers = X[cluster_centers_indices]
+
+    # 3b. Replace cluster_center_indices by integers from 0 to num_centers - 1; Update labels
+    mapping = np.arange(0, max(final_labels) + 1)
+    mapping[list(cluster_centers_indices)] = list(range(len(cluster_centers_indices)))
+    final_labels = mapping[final_labels]
+
+    return cluster_centers_indices, cluster_centers, final_labels
+
+def update_responsibility(similarities_mat: NDArray,
+                          responsibilities_mat: NDArray,
+                          availabilities_mat: NDArray,
+                          damping: float = 0.5) -> NDArray:
+    """Compute values for the responsibility messages for the next iteration.
+    Parameters:
+        similarities_mat:       Array of similarity values; Shape `(n, n)`
+        responsibilities_mat:   Array of responsibilities from previous iteration; Shape `(n, n)`
+        availabilities_mat:     Array of availabilities from previous iteration; Shape `(n, n)`
+        damping:                Damping factor to avoid numerical oscillations between iterations.
+                                0: Only new responsibility values will be used
+                                1: Only old responsibility values will be used (no update)
+                                Recommended interval is [0.5, 1.0)
+    Returns:
+        Array of shape `(n, n)` containing the new responsibility values.
+    """
     # 1. Add availability and similarity for all exemplars (excluding self-reference [the diagonal])
     sum_mat = availabilities_mat + similarities_mat
     np.fill_diagonal(sum_mat, -np.inf)
@@ -45,19 +118,19 @@ def compute_responsibility(similarities_mat: NDArray,
     return (1 - damping) * new_responsibility_mat + damping * responsibilities_mat
 
 
-def compute_availability(responsibilities_mat: NDArray,
-                         availabilities_mat: NDArray,
-                         damping: float = 0.5) -> NDArray:
-    """ Computes values for the availability messages for the next iteration.
-        Parameters:
-            responsibilities_mat:   Array of responsibilities from previous iteration; Shape `(n, n)`
-            availabilities_mat:     Array of availabilities from previous iteration; Shape `(n, n)`
-            damping:                Damping factor to avoid numerical oscillations between iterations.
-                                    0: Only new responsibility values will be used
-                                    1: Only old responsibility values will be used (no update)
-                                    Recommended range [0.5, 1.0)
-        Returns:
-            Array of shape `(n, n)` containing the new availability values.
+def update_availability(responsibilities_mat: NDArray,
+                        availabilities_mat: NDArray,
+                        damping: float = 0.5) -> NDArray:
+    """Compute values for the availability messages for the next iteration.
+    Parameters:
+        responsibilities_mat:   Array of responsibilities from previous iteration; Shape `(n, n)`
+        availabilities_mat:     Array of availabilities from previous iteration; Shape `(n, n)`
+        damping:                Damping factor to avoid numerical oscillations between iterations.
+                                0: Only new responsibility values will be used
+                                1: Only old responsibility values will be used (no update)
+                                Recommended range [0.5, 1.0)
+    Returns:
+        Array of shape `(n, n)` containing the new availability values.
     """
     # 1. Create a temporary matrix to find the sum of responsibilities for each exemplar to points other than itself
     # - Only consider non-negative values in the sum    -> Set negative values to 0
@@ -84,7 +157,7 @@ def compute_availability(responsibilities_mat: NDArray,
 def compute_similarity(X: np.ndarray, precomputed: Optional[NDArray] = None) -> NDArray:
     """Compute similarity values between points in array X. Default are negative Euclidean distances
     Parameters:
-        X:              Array with n rows (i.e. points)
+        X:              Two-dimensional array of shape `(n, m)` (n = number of points; m = number of features)
         precomputed:    Optional array of shape (n, n) to provide precomputed similarities
 
     Returns:
@@ -100,9 +173,9 @@ def compute_similarity(X: np.ndarray, precomputed: Optional[NDArray] = None) -> 
 
 
 def set_preferences(similarities_mat: np.ndarray, preferences: Optional[NDArray] = None) -> NDArray:
-    """Updates the similarity_matrix with preferences values
+    """Update the similarity_matrix with preferences values
     Parameters:
-        similarities_mat:  Array of shape (n, n) containing similarities between n points
+        similarities_mat:   Array of shape (n, n) containing similarities between n points
         preferences:        Optional array of shape (n, ); If None, the median of all similarities is used
 
     Returns:
@@ -118,3 +191,22 @@ def set_preferences(similarities_mat: np.ndarray, preferences: Optional[NDArray]
     # Update self similarity values and return updated matrix
     np.fill_diagonal(similarities_mat, preferences)
     return similarities_mat
+
+def prepare_matrices(X: NDArray, precomputed: Optional[NDArray] = None,
+                     preferences: Optional[NDArray] = None) -> Tuple[NDArray, NDArray, NDArray]:
+    """Prepare the matrices for similarity, responsibility, and availability
+    Parameters:
+        X:              Two-dimensional array of shape `(n, m)` (n = number of points; m = number of features)
+        precomputed:    Optional array of shape (n, n) to provide precomputed similarities
+        preferences:    Optional array of shape (n, ); If None, the median of all similarities is used
+
+    Returns:
+                Three arrays of shape (n, n): Similarities, Responsibilities, Availabilities
+    """
+    similarities_mat = compute_similarity(X, precomputed)
+    similarities_mat = set_preferences(similarities_mat, preferences=preferences)
+    # Adapt similarity_mat to remove any degeneracies that might cause oscillation
+    similarities_mat = similarities_mat + 1e-12 * np.random.normal(size=similarities_mat.shape) \
+                     * (np.max(similarities_mat) - np.min(similarities_mat))
+
+    return similarities_mat, np.zeros_like(similarities_mat), np.zeros_like(similarities_mat)
