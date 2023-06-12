0c1c033281027d3d03a6ed18146c81c8fa382b3c
Harpreet Int-Veen <int-veen@MacBook-Pro-von-Harpreet.local>
2023-06-12 10:48:02 +0200

    change data import to function

--- Group5/dataimport.py
+++ Group5/dataimport.py
@@ -2,69 +2,71 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os  # operating system
 
-# Task 1: Load data sets
-data_directory = "data"
-dataframe = pd.read_csv(os.path.join(data_directory, "data.csv"))
-print(dataframe.head(10))
-print(dataframe.describe())
-print(dataframe.shape)
 
-data_directory_labels = "data_labels"
-dataframe_labels = pd.read_csv(os.path.join(data_directory, "labels.csv"))
-print(dataframe_labels.head(20))
-print(dataframe_labels.describe())
-print(dataframe_labels.shape)
+def data_preparation():
+    # Task 1: Load data sets
+    data_directory = "data"
+    dataframe = pd.read_csv(os.path.join(data_directory, "data.csv"))
+    print(dataframe.head(10))
+    print(dataframe.describe())
+    print(dataframe.shape)
 
-# dataframe['cancer_type'] = dataframe_labels['Class']
+    data_directory_labels = "data_labels"
+    dataframe_labels = pd.read_csv(os.path.join(data_directory, "labels.csv"))
+    print(dataframe_labels.head(20))
+    print(dataframe_labels.describe())
+    print(dataframe_labels.shape)
 
-# Task 2: Merge the data sets
+    # dataframe['cancer_type'] = dataframe_labels['Class']
 
-dataframe = dataframe_labels.merge(dataframe, how='inner', on='Unnamed: 0')
-print(dataframe.head(10))
-print(dataframe.describe())
-print(dataframe.shape)
+    # Task 2: Merge the data sets
 
-print(dataframe.isnull().sum())
+    dataframe = dataframe_labels.merge(dataframe, how='inner', on='Unnamed: 0')
+    print(dataframe.head(10))
+    print(dataframe.describe())
+    print(dataframe.shape)
 
-# Task 3: Descriptive statistics of the data
+    print(dataframe.isnull().sum())
 
-# Bar plot of the five different types of cancer
+    # Task 3: Descriptive statistics of the data
 
-dataframe['Class'].value_counts().plot(kind='bar', xlabel='cancer types',
-                                       ylabel='frequency', title='Distribution of cancer types',
-                                       color='green', figsize=(6, 7))
-plt.plot()
-plt.savefig('Distribution of cancer types.png')
-plt.show()
+    # Bar plot of the five different types of cancer
 
-# Relative distribution of the five different cancer types
-(dataframe['Class'].value_counts()/len(dataframe)).plot(kind='bar', xlabel='cancer types',
-                                                        ylabel='frequency',
-                                                        title='Relative distribution of cancer types',
-                                                        color='green', figsize=(6, 7))
+    dataframe['Class'].value_counts().plot(kind='bar', xlabel='cancer types',
+                                           ylabel='frequency', title='Distribution of cancer types',
+                                           color='green', figsize=(6, 7))
+    plt.plot()
+    plt.savefig('Distribution of cancer types.png')
+    plt.show()
 
-plt.plot()
-plt.savefig('Relative distribution of cancer types.png')
-plt.show()
+    # Relative distribution of the five different cancer types
+    (dataframe['Class'].value_counts() / len(dataframe)).plot(kind='bar', xlabel='cancer types',
+                                                              ylabel='frequency',
+                                                              title='Relative distribution of cancer types',
+                                                              color='green', figsize=(6, 7))
 
-# kernel density estimation (KDE) to estimate the probability density function of a
-# random variable
-# dataframe.groupby('Class')['gene_1'].plot.kde()
-# plt.plot()
-# plt.show()
+    plt.plot()
+    plt.savefig('Relative distribution of cancer types.png')
+    plt.show()
 
-# plotting the density of the mean values of all genes
-fig, ax = plt.subplots(figsize=(8, 6))
-df = pd.DataFrame({'Class': dataframe['Class'],
-                   'Density': dataframe.iloc[:, 2:].sum(axis=1)/len(dataframe.columns[2:])})
-labels = []
-for label, df_grouped in df.groupby('Class'):
-    labels.append(label)
-    df_grouped.plot(kind="kde", ax=ax)
+    # kernel density estimation (KDE) to estimate the probability density function of a
+    # random variable
+    # dataframe.groupby('Class')['gene_1'].plot.kde()
+    # plt.plot()
+    # plt.show()
 
-plt.legend(labels)
-plt.title('Density plot of the mean M of all the genes')
-plt.xlabel('Mean M')
-plt.plot()
-plt.savefig('Density plot of the mean M of all the genes.png')
-plt.show()
+    # plotting the density of the mean values of all genes
+    fig, ax = plt.subplots(figsize=(8, 6))
+    df = pd.DataFrame({'Class': dataframe['Class'],
+                       'Density': dataframe.iloc[:, 2:].sum(axis=1) / len(dataframe.columns[2:])})
+    labels = []
+    for label, df_grouped in df.groupby('Class'):
+        labels.append(label)
+        df_grouped.plot(kind="kde", ax=ax)
+
+    plt.legend(labels)
+    plt.title('Density plot of the mean M of all the genes')
+    plt.xlabel('Mean M')
+    plt.plot()
+    plt.savefig('Density plot of the mean M of all the genes.png')
+    plt.show()
