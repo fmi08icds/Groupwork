b3384065140daad1478f7fd0a0156df5d8151690
marco <marco-hunter@web.de>
2023-06-26 16:50:34 +0200

    update main.py add eval.py with training and evaluation methods update svm_test.py for measuring time update sklearn_eval.py with temporary placeholder for preprocessing

--- Group2/src/main.py
+++ Group2/src/main.py
@@ -1,39 +1,16 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from numpy import sum, ndarray
+from eval import train_svm_model
+from sklearn_eval import preprocess_data  ####### Replace with preprocessing DataAnalyzer as soon as its finished
 
-from smo_svm import SVM
 
-def accuracy(predicted_y: ndarray, y: ndarray):
-    n = y.shape[0]
-    assert predicted_y.shape[0] == n
-    return sum(predicted_y == y) / n
+def main():
+    file = '../data/diabetes_prediction_dataset.csv'
+    data = pd.read_csv(file)
+    X_train, X_val, X_test, y_train, y_val, y_test = preprocess_data(data)  ####### Replace with preprocessing DataAnalyzer as soon as its finished
+    #train_svm_model("Sklearn", X_train, X_val, X_test, y_train, y_val, y_test)
+    #train_svm_model("SMO", X_train, X_val, X_test, y_train, y_val, y_test)
+    train_svm_model("HardMargin", X_train, X_val, X_test, y_train, y_val, y_test)
 
 
 if __name__ == "__main__":
-    # Load the data
-    df = pd.read_csv("Group2/data/diabetes_prediction_dataset.csv")
-    df = df.sample(1000)
-    df["diabetes"].replace(0, -1, inplace=True)
-
-    train_df, test_df = train_test_split(df, test_size=0.2)
-
-    # Split the data into labels and features
-    train_labels, test_labels = train_df["diabetes"], test_df["diabetes"]
-    train_features, test_features = train_df.drop("diabetes", axis=1), test_df.drop("diabetes", axis=1)
-
-    # Use only some numeric columns for quick svm testing purposes
-    sample_columns = ["bmi", "blood_glucose_level"]
-    sample_test_features = test_features[sample_columns]
-    sample_train_features = train_features[sample_columns]
-
-    # # Fit SVM
-    svm = SVM()
-    svm.fit(sample_train_features.to_numpy(), train_labels.to_numpy())
-    # svm = train_new_svm(sample_train_features.to_numpy(), train_labels.to_numpy())
-
-    predicted_test_labels = svm.predict(sample_test_features)
-    print("predicted_test_labels", predicted_test_labels)
-    print("test_labels", test_labels.to_numpy())
-    print(pd.concat((test_labels, predicted_test_labels - test_labels), axis=1))
-    print(f"accuracy: {accuracy(predicted_test_labels, test_labels):.4f}")
+    main()
