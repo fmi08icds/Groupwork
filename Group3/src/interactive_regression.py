2ba0c0969de3f51efa884abc64592c45b77b3df4
pierreachkar1717 <pa27cufo@studserv.uni-leipzig.de>
2023-06-27 19:20:48 +0200

    updatedet layout and formation

--- Group3/src/interactive_regression.py
+++ Group3/src/interactive_regression.py
@@ -2,12 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class InteractiveRegression:
-    """ 
+    """
     This class is used to let the user interactively change the coefficients and the intercept of a regression model.
     """
 
-# init method or constructor
+    # init method or constructor
     def __init__(self, x_data, y_data, b0, b1, b2):
         self.x_data = x_data
         self.y_data = y_data
@@ -19,23 +20,24 @@ class InteractiveRegression:
 
     # method for calculating the predicted values
     def calc_predicted_values(self):
-        self.predicted_values = self.b0 + self.b1 * self.x_data + self.b2 * self.x_data**2
+        self.predicted_values = (
+            self.b0 + self.b1 * self.x_data + self.b2 * self.x_data**2
+        )
         return self.predicted_values
 
     # method for calculating the residuals
     def calc_residuals(self):
         self.residuals = self.y_data - self.predicted_values
         return self.residuals
-    
+
     # method for calculating the sum of squares
     def calc_sum_of_squares(self):
-        return np.sum(self.residuals**2)
-    
+        return round(np.sum(self.residuals**2), 2)
+
     # method for calculating the mean squared error
     def calc_mean_squared_error(self):
-        return np.mean(self.residuals**2)
-    
+        return round(np.mean(self.residuals**2), 2)
+
     # method for calculating the root mean squared error
     def calc_root_mean_squared_error(self):
-        return np.sqrt(self.calc_mean_squared_error())
-    
+        return round(np.sqrt(self.calc_mean_squared_error()), 2)
