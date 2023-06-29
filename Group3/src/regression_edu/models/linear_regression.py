99c772ad7fd52e4fc11d8266adf31b57607bc47d
philayzen <philayzen@gmail.com>
2023-06-29 09:27:02 +0200

    Added MSE and MAE. Added function to get the column of the x-data

--- Group3/src/regression_edu/models/linear_regression.py
+++ Group3/src/regression_edu/models/linear_regression.py
@@ -2,14 +2,18 @@ import math
 
 import numpy as np
 
-
 class LinearRegression:
     coeffs = None
     predicted_values = None
     x_data = None
     y_data = None
+    name = ""
 
-    def __init__(self, data, transposed=False):
+    # def __init__(self, data, transposed=False, name=""):
+    #     self.calc(data, transposed, name)
+
+
+    def __init__(self, data, transposed=False, name=""):
         """
         Calculates the ordinary linear regression for the given data.
         :param data: data is a Nx(d+1) matrix with the last column being Y and X being Nxd. data[0] accesses therefor the first sample.
@@ -23,17 +27,30 @@ class LinearRegression:
         # removes y-column and ads column of ones for the bias (w0)
         self.x_data = data[:,:-1]
         x_data = np.asarray([np.insert(sample, 0, 1) for sample in self.x_data])
-
         self.coeffs = np.linalg.pinv(np.transpose(x_data) @ x_data) @ np.transpose(x_data) @ self.y_data
         self.predicted_values = x_data @ self.coeffs
+        self.name = ""
 
     def f(self, x):
-        x = np.asarray([x])
-        if len(x) != len(self.coeffs) - 1:
+        if type(x) is not np.array(()):
+            if type(x) in [int, float]:
+                x = [x]
+            x = np.asarray(x)
+        if len(x) != len(self.coeffs[0]) - 1:
             raise ValueError(
                 f"x has to have the same dimension as x_data. dim x: {len(x)}; dim x_data: {len(self.x_data[0])}")
         return sum(self.coeffs * np.insert(x, 0, 1))
 
+    def get_x_column(self, i):
+        return self.x_data[:,i]
 
     def get_sum_of_squares(self):
         return np.sum([(self.predicted_values[i] - self.y_data[i]) ** 2 for i in range(len(self.y_data))])
+
+
+    def get_MSE(self):
+        return self.get_sum_of_squares()/len(self.y_data)
+
+
+    def get_MAE(self):
+        return np.sum([abs(self.predicted_values[i] - self.y_data[i]) for i in range(len(self.y_data))]) / len(self.y_data)
