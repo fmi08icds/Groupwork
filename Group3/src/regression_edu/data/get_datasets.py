fbbe86e2b42b6f1bc705da8a9b4abe7a1b97111e
tomashkin <tomislav.popov98@gmail.com>
2023-07-01 00:14:29 +0200

    Completed regression_comparison, added dependencies, fixed small bug on lwr.py

--- Group3/src/regression_edu/data/get_datasets.py
+++ Group3/src/regression_edu/data/get_datasets.py
@@ -1,43 +1,45 @@
+# dependencies: imports + openpyxl (for converting to excel)
 import urllib.request
 import zipfile
 import os
 
+url_base = "./realworld/"
 
 ## 1. Real Estate Valuation Data Set
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00477/Real%20estate%20valuation%20data%20set.xlsx"
-filename = "./Groupwork/Group3/data/realworld/real_estate.xlsx"
+filename = url_base + "real_estate.xlsx"
 urllib.request.urlretrieve(url, filename)
 
 
 ## 2. Wine Quality Data Set
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-filename = "./Groupwork/Group3/data/realworld/winequality-red.csv"
+filename = url_base + "winequality-red.csv"
 urllib.request.urlretrieve(url, filename)
 
-## 3. Air Quality Data Set
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00360/AirQualityUCI.zip"
-filename = "./Groupwork/Group3/data/realworld/AirQualityUCI.zip"
-urllib.request.urlretrieve(url, filename)
-
-# unpack air quality data
-with zipfile.ZipFile(filename, 'r') as zip_ref:
-    zip_ref.extractall("./Groupwork/Group3/data/realworld/")
-
-## remove zip file
-os.remove(filename)
-
-## remove duplicate
-os.remove("./Groupwork/Group3/data/realworld/AirQualityUCI.csv")
+# ## 3. Air Quality Data Set
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00360/AirQualityUCI.zip"
+# filename = "./Groupwork/Group3/data/realworld/AirQualityUCI.zip"
+# urllib.request.urlretrieve(url, filename)
+#
+# # unpack air quality data
+# with zipfile.ZipFile(filename, 'r') as zip_ref:
+#     zip_ref.extractall("./Groupwork/Group3/data/realworld/")
+#
+# ## remove zip file
+# os.remove(filename)
+#
+# ## remove duplicate
+# os.remove("./Groupwork/Group3/data/realworld/AirQualityUCI.csv")
 
 ## 4. prostate
 url = "https://hastie.su.domains/ElemStatLearn/datasets/prostate.data"
-filename = "./Groupwork/Group3/data/realworld/prostate.data"
+filename = url_base + "prostate.data"
 urllib.request.urlretrieve(url, filename)
 
 ## convert .data to .xlsx
 import pandas as pd
-df = pd.read_csv("./Groupwork/Group3/data/realworld/prostate.data", sep='\t')
-df.to_excel("./Groupwork/Group3/data/realworld/prostate.xlsx", index=False)
+df = pd.read_csv(url_base + "prostate.data", sep='\t')
+df.to_excel(url_base + "prostate.xlsx", index=False)
 os.remove(filename)
 
 
