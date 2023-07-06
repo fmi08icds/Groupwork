78e11ac3d62764267e979a8bbf50d050627a8957
Jerome Wuerf <oss@scharom.de>
2023-07-06 20:06:17 +0200

    Improve code quality

--- Group3/src/regression_edu/models/linear_regression.py
+++ Group3/src/regression_edu/models/linear_regression.py
@@ -1,79 +1,47 @@
-import math
-
+"""This module contains the LinearRegression model"""
 import numpy as np
+from regression_edu.models.base_regression import BaseRegression
 
 
-class LinearRegression:
-    coeffs = None
-    predicted_values = None
-    x_data = None
-    y_data = None
-    name = ""
+class LinearRegression(BaseRegression):
+    """
+    This class implements a linear regression using ordinary least squares.
+    """
 
-    def __init__(self, data, transposed=False, name=""):
+    def __init__(self, data, transposed, name):
         """
         Calculates the ordinary linear regression for the given data.
         :param data: data is a Nx(d+1) matrix with the last column being Y and
             X being Nxd. data[0] accesses therefor the first sample.
         :param transposed: If the data is transposed and data[0] returns a
-            vector of the first dimension of the samples.
-        :param name: Optional parameter to pass a name.
         """
-        data = np.asarray(data)
-        if transposed:
-            data = np.transpose(data)
-
-        self.y_data = data[:, -1]
-        # removes y-column and ads column of ones for the bias (w0)
-        self.x_data = data[:, :-1]
+        super().__init__(data, transposed, name)
         x_data = np.asarray([np.insert(sample, 0, 1) for sample in self.x_data])
-        self.coeffs = (
+        self.coefficients = (
             np.linalg.pinv(np.transpose(x_data) @ x_data)
             @ np.transpose(x_data)
             @ self.y_data
         )
-        self.predicted_values = x_data @ self.coeffs
-        self.name = ""
+        self.predicted_values = x_data @ self.coefficients
 
-    def f(self, x):
+    def predict(self, x):
         """
-        Calculates the prediction for a given datapoint. It doesn't support the input of
-        multiple datapoints.
+        Calculates the prediction for a given data point. It doesn't support the input
+        of multiple data points.
 
         :param x: A numeric value or vector consisting of one value for each factor of
             the data point
         :return: returns a float as the prediction for the given data point.
+        :raises ValueError
         """
-        if type(x) is not np.array(()):
+        if not isinstance(x, np.ndarray):
             if type(x) in [int, float]:
                 x = [x]
             x = np.asarray(x)
         length = len(x) if np.ndim(x) != 0 else 1
-        if length != len(self.coeffs) - 1:
+        if length != len(self.coefficients) - 1:
             raise ValueError(
                 f"x has to have the same dimension as x_data. dim x: {len(x)}; \
 dim x_data: {len(self.x_data[0])}"
             )
-        return sum(self.coeffs * np.insert(x, 0, 1))
-
-    def get_x_column(self, i):
-        return self.x_data[:, i]
-
-    def get_sum_of_squares(self):
-        return np.sum(
-            [
-                (self.predicted_values[i] - self.y_data[i]) ** 2
-                for i in range(len(self.y_data))
-            ]
-        )
-
-    def get_MSE(self):
-        return self.get_sum_of_squares() / len(self.y_data)
-
-    def get_MAE(self):
-        return np.sum(
-            [
-                abs(self.predicted_values[i] - self.y_data[i])
-                for i in range(len(self.y_data))
-            ]
-        ) / len(self.y_data)
+        return sum(self.coefficients * np.insert(x, 0, 1))
--- Group3/src/regression_edu/visualization/plot.py
+++ Group3/src/regression_edu/visualization/plot.py
@@ -1,36 +1,11 @@
+"""The functions in this module serve as a quick sanity check to evaluate the
+output of our regression classes visually with out the need of notebooks or 
+visualization engines like dash"""
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def plot_2d(reg, show_predictands=False, show_residuals=True):
-    """
-    Plot data with one dependent and one independent variable to show a 2D plot.
-    :param reg: An instance of a regression from a class in regression.py
-    :param show_predictands: By default true. If true, it marks the predicted values for the x_data with an x on the line
-    :param show_residuals: By default true. If true, is shows the residuals with red, dotted lines.
-    :return: None
-    """
-    x_data = reg.x_data
-    y_data = reg.y_data
-    predicted = reg.predicted_values
-
-    plt.figure("Regression " + reg.name)
-    plt.plot(x_data, y_data, "rx", markersize=3)
-    if show_predictands:
-        plt.plot(x_data, predicted, "bx", markersize=3)
-    if show_residuals:
-        [
-            plt.plot((x_data[i], x_data[i]), (y_data[i], predicted[i]), "r--")
-            for i in range(len(predicted))
-        ]
-    # division by 5 was arbitrarily chosen
-    margin = (max(x_data) - min(x_data)) / 5
-    linspace = np.linspace(min(x_data) - margin, max(x_data) + margin)
-    plt.plot(linspace, [reg.f(xi) for xi in linspace], "k-")
-    # plt.show()
-
-
-def plot_3d(reg, show_predictands=False, show_residuals=True):
     """
     Plot data with one dependent and one independent variable to show a 2D plot.
     :param reg: An instance of a regression from a class in regression.py
@@ -45,13 +20,36 @@ def plot_3d(reg, show_predictands=False, show_residuals=True):
     predicted = reg.predicted_values
 
     plt.figure("Regression " + reg.name)
+    plt.plot(x_data, y_data, "rx", markersize=3)
+    if show_predictands:
+        plt.plot(x_data, predicted, "bx", markersize=3)
+    if show_residuals:
+        for i in enumerate(predicted):
+            plt.plot((x_data[i], x_data[i]), (y_data[i], predicted[i]), "r--")
+    # division by 5 was arbitrarily chosen
+    margin = (max(x_data) - min(x_data)) / 5
+    linspace = np.linspace(min(x_data) - margin, max(x_data) + margin)
+    plt.plot(linspace, [reg.f(xi) for xi in linspace], "k-")
+    # plt.show()
+
+
+def plot_3d(reg):
+    """
+    Plot data with one dependent and one independent variable to show a 2D plot.
+    :param reg: An instance of a regression from a class in regression.py
+    :param show_predictands: By default true. If true, it marks the predicted values
+        for the x_data with an x on the line
+    :param show_residuals: By default true. If true, is shows the residuals with red,
+        dotted lines.
+    :return: None
+    """
+    y_data = reg.y_data
+
+    plt.figure("Regression " + reg.name)
     ax = plt.subplot(projection="3d")
     ax.scatter(reg.get_x_column(0), reg.get_x_column(1), y_data, marker="x")
-    # if show_predictands:
-    #     plt.plot(x_data, predicted, "bx", markersize=3)
-    # if show_residuals:
-    #     [plt.plot((x_data[i], x_data[i]), (y_data[i], predicted[i]), "r--") for i in range(len(predicted))]
-    # division by 5 was arbitrarily chosen
+
+    # division by 5 was chosen by convention
     margin_x1 = max(reg.get_x_column(0)) - min(reg.get_x_column(0)) / 5
     margin_x2 = max(reg.get_x_column(0)) - min(reg.get_x_column(0)) / 5
     linspace_x1 = np.linspace(
@@ -61,40 +59,38 @@ def plot_3d(reg, show_predictands=False, show_residuals=True):
         min(reg.get_x_column(1)) - margin_x2, max(reg.get_x_column(1)) + margin_x2
     )
     x_lin, y_lin = np.meshgrid(linspace_x1, linspace_x2)
-    # plt.plot(x_lin, y_lin, [[reg.f([xi,yi]) for yi in y_lin] for xi in x_lin])
-    print(x_lin.shape[0])
     z = np.asarray(
         [
             [reg.f([x_lin[i, j], y_lin[i, j]]) for j in range(x_lin.shape[1])]
             for i in range(x_lin.shape[0])
         ]
     )
-    print(z.shape)
-    # cont = ax.contour(x_lin, y_lin, z, 20)
-    # plt.colorbar(cont, shrink=.5, aspect=10)
     ax.plot_surface(
         x_lin, y_lin, z, edgecolor="royalblue", lw=0.5, rstride=8, cstride=8, alpha=0.3
     )
-    # plt.show()
 
 
 def plot_gaussians(reg, x):
+    """
+    Plots the gaussians
+
+    :param reg: The regression model
+    :param x: Ths point to evaluate
+    """
     plt.figure("Gaussians " + reg.name)
     summed_gauss = sum(
-        [
-            reg.gauss(reg.centres[index], x, reg.sigma)
-            for index in range(len(reg.coeffs))
-        ]
+        reg.gauss(reg.centres[index], x, reg.sigma) for index in range(len(reg.coeffs))
     )
-    [
+    for ci in reg.centres:
         plt.plot(x, [reg.gauss(ci, xi, reg.sigma) / summed_gauss for xi in x])
-        for ci in reg.centres
-    ]
 
 
 def plot_residuals(reg):
-    x_data = reg.x_data
-    y_data = reg.y_data
+    """
+    Plots the residuals of the given regression model
+
+    :param reg: The regression model
+    """
     plt.figure("Residuals " + reg.name)
     # create residual presentation
     plt.plot(x=reg.y_data, y=reg.predicted_values, markersize=3)
